59ba25eb7bc3f7e647033379fedbc1738a8bd2bd
jatin0493 <65082215+jatin0493@users.noreply.github.com>
2020-06-18 22:41:07 +0530

    Update sort.py

--- sort.py
+++ sort.py
@@ -9,3 +9,9 @@ def sort_photos(file_names):
         first_part = re.split('[^a-zA-Z]', file_name)[0]
         dct[first_part].append(file_name)
     print(dct)
+files = ['mallika_1.jpg', 'dog005.jpg', 'grandson_2018_01_01.png', 'dog008.jpg', 'mallika_6.jpg', 'grandson_2018_5_23.png', 'dog01.png', 'mallika_11.jpg', 'mallika2.jpg', 'grandson_2018_02_5.png', 'grandson_2019_08_23.jpg', 'dog9.jpg', 'mallika05.jpg' ]
+
+sort_photos(files)
+
+
+## Output =  {'mallika': ['mallika_1.jpg', 'mallika_6.jpg', 'mallika_11.jpg', 'mallika2.jpg', 'mallika05.jpg'], 'dog': ['dog005.jpg', 'dog008.jpg', 'dog01.png', 'dog9.jpg'], 'grandson': ['grandson_2018_01_01.png', 'grandson_2018_5_23.png', 'grandson_2018_02_5.png', 'grandson_2019_08_23.jpg']}
