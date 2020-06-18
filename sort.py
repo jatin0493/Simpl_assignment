c61d2f3596e87a89da28360a8bd8dcc27a3cdb64
jatin0493 <65082215+jatin0493@users.noreply.github.com>
2020-06-18 22:28:24 +0530

    Update sort.py

--- sort.py
+++ sort.py
@@ -1,1 +1,11 @@
+import re
+from collections import defaultdict
 
+dct = defaultdict(list)
+
+
+def sort_photos(file_names):
+    for file_name in file_names:
+        first_part = re.split('[^a-zA-Z]', file_name)[0]
+        dct[first_part].append(file_name)
+    print(dct)
